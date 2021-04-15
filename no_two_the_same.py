202a761b2804e31c4005916680973a557a286094
j-o-ncl <56452899+j-o-ncl@users.noreply.github.com>
2021-04-15 10:45:08 +0100

    multiple cards now available :)

--- no_two_the_same.py
+++ no_two_the_same.py
@@ -1,15 +1,24 @@
 #%%
 import random
 import numpy as np
+
+cards = int(input('How many cards would you like to draw? '))
+
+print('Drawing {} cards....'.format(cards))
+
 a = 1
 b = 14
 
-two_random_cards = random.sample(range(a, b+1), 2)
+random_cards = random.sample(range(a, b+1), cards)
 
-player_1 = two_random_cards[0]
-player_2 = two_random_cards[1]
+for card in (random_cards):
+    print('You drew card {}'.format(card))
 
-print(player_1)
-print(player_2)
+player_card =int(input('Which card would you like to play?'))
+
+if player_card not in random_cards:
+    print('Not valid! You did not draw that card!')
+else:
+    print('Need to integrate with base code :)')
 
 # %%
