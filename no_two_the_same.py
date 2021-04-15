764f77c0db9d17fe1709325a00fd5a000b091beb
j-o-ncl <56452899+j-o-ncl@users.noreply.github.com>
2021-04-15 09:56:56 +0100

    simple code to eliminate duplicate cards :)

---
+++ no_two_the_same.py
@@ -0,0 +1,15 @@
+#%%
+import random
+import numpy as np
+a = 1
+b = 14
+
+two_random_cards = random.sample(range(a, b+1), 2)
+
+player_1 = two_random_cards[0]
+player_2 = two_random_cards[1]
+
+print(player_1)
+print(player_2)
+
+# %%
